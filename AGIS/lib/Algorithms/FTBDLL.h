99babf76a85d651c31a89310e1dd06358c323195
Tnzr <bryan.garcia.b9r@gmail.com>
2023-04-27 00:22:12 -0400

    Front To Back Doubly Linked List Implementation. Important Note: the template Implementation is included in the header at the bottom

--- AGIS/lib/Algorithms/FTBDLL.h
+++ AGIS/lib/Algorithms/FTBDLL.h
@@ -1,6 +1,10 @@
 #pragma once
+#include <Arduino.h>
 #include <Node.h>
-template<typename dType> class FTBDLL{ // Front to Back Doubly Linked List
+
+
+template<typename dType> 
+class FTBDLL{ // Front to Back Doubly Linked List
     private:
         Node<dType> *head_node;
         Node<dType> *present;
@@ -8,16 +12,19 @@ template<typename dType> class FTBDLL{ // Front to Back Doubly Linked List
         int n_reserved; // reserved instances
         int size; // in use
     public:
-        FTBDLL(int reserve);
+        FTBDLL(int initial_size=1);
         ~FTBDLL();
-        void reserve(int reserve_size);
-        virtual void display();
+        void reserve(int reserve_size=1);
+        void display();
         bool push_back(dType data); // after last valid node
         bool push_front(dType data); // before head node
         void clear(Node<dType> *node); // clear data and stitch sorrounding nodes
         void clear(Node<dType> *older_node, Node<dType> *recent_node); // 
         void detach(Node<dType> *node); // clear data and stitch sorrounding
         void detach(Node<dType> *older_node, Node<dType> *recent_node);
-
+        bool isFull();
+        int getSize();
 
 };
+
+#include "FTBDLL.cpp"
